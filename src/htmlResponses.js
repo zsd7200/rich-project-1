50a578310c647ad5ccda6bfb46100060e49a18c6
Zack D <zsd7200@rit.edu>
2019-10-12 18:37:42 -0400

    add favicon, change name (might change it again though idk yet)

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,6 +4,7 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/style.css`);
 const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
+const favicon = fs.readFileSync(`${__dirname}/../hosted/favicon.png`);
 
 // get index/css/bundle
 const getIndex = (request, response) => {
@@ -24,8 +25,15 @@ const getBundle = (request, response) => {
   response.end();
 };
 
+const getFavicon = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(favicon);
+  response.end();
+};
+
 module.exports = {
   getIndex,
   getCSS,
   getBundle,
+  getFavicon,
 };
--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const urlStruct = {
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getCSS,
     '/bundle.js': htmlHandler.getBundle,
+    '/favicon.png' : htmlHandler.getFavicon,
     '/pokemon': jsonHandler.getPokemon,
     notFound: jsonHandler.notFound,
   },
